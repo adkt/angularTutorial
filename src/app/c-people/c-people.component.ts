bc593ac464f803cb5f2a7cc7eec0baa72f06e7c9
adkt <adkt@outlook.com>
2019-03-20 15:39:43 +1100

    Trying to import json

--- src/app/c-people/c-people.component.ts
+++ src/app/c-people/c-people.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from '../person'
+import dataPeople from '../mock-people.json';
+
 @Component({
   selector: 'app-c-people',
   templateUrl: './c-people.component.html',
@@ -7,6 +9,8 @@ import { Person } from '../person'
 })
 export class CPeopleComponent implements OnInit {
 
+  testname = (<any>dataPeople).Group[0].Name;
+  
   person1: Person = {
     id: 1,
     name: 'Windstorm'

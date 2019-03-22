e850e6d3bb206565d36ebaa7827b228cde99b94a
adkt <adkt@outlook.com>
2019-03-22 12:48:11 +1100

    How to detect Clicking outside Person details

--- src/app/c-people-details/c-people-details.component.ts
+++ src/app/c-people-details/c-people-details.component.ts
@@ -1,5 +1,5 @@
 // added Input so we can use @Input Property. An @Input property is a property that bound to another component
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 // added Person class for this component
 import { Person } from '../person'
@@ -9,13 +9,33 @@ import { Person } from '../person'
   templateUrl: './c-people-details.component.html',
   styleUrls: ['./c-people-details.component.css']
 })
+
 export class CPeopleDetailsComponent implements OnInit {
 
+  /*** Directives ***/
   // Here we bind the person property, declare Input Properties in the source component where it adds this components element [person]="linkedVariableNameFromSourceComponent"
   @Input() selectedPerson: Person;
   @Input() allPeople;
+
+  // HostListener listens for events then certain functions (e.g. clickout) run when the event is triggered
+   @HostListener('click')
+  clickInside() {
+    console.log('clicked inside');
+    this.wasInside = true;
+  }
   
-  constructor() { }
+  @HostListener('document:click')
+  clickOutside() {
+    if (!this.wasInside) {
+      console.log('clicked outside');
+    }
+    this.wasInside = false;
+  }
+
+  /*** vars ***/
+  private wasInside = false;
+
+  constructor( private eRef: ElementRef ) { }
 
   ngOnInit() {
   }
